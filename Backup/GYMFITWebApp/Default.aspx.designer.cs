a10b962777e5acfca79c2d412dc6d94f1de9bd60
taterazzy <taterazzy@gmail.com>
2016-11-07 18:42:58 +0800

    Adding page i guess?
    Add aspx pages

---
+++ Backup/GYMFITWebApp/Default.aspx.designer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace GYMFITWebApp
+{
+
+  public partial class Default
+  {
+    protected System.Web.UI.WebControls.Button button1;
+  }
+}
